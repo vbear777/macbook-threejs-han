ed396f87e3eb28b5d647bf21ad09923598db2564
HanVbear <ryoitenkaii@gmail.com>
2025-12-29 10:22:26 +0700

    add SwitchColor to macbook models

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import ProductViewer from "./components/ProductViewer";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import ShowCase from "./components/Showcase";
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
             <NavBar />
             <Hero />
             <ProductViewer />
+            <ShowCase />
         </main>
     )
 }
--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -2,7 +2,54 @@ const ShowCase = () => {
     return (
         <section id="showcase">
             <div className="media">
-                <video src=""></video>
+                <video src="/videos/game.mp4" loop muted playsInline />
+                <div className="mask">
+                    <img src="/mask-logo.svg" alt="apple mask logo" />
+                </div>
+            </div>
+
+            <div className="content">
+                <div className="wrapper">
+                    <div className="lg:max-w-md">
+                        <h2>
+                            Follow the Wind
+                        </h2>
+
+                        <div className="space-y-5 mt-7 pe-10">
+                            <p>
+                                Introducing {""}
+                                <span className="text-white">
+                                    M4, The Next Gen
+                                </span>
+                                . M4 technology
+                            </p>
+                            <p>
+                                Designed for the future, Apple M4 provides a faster, smarter, 
+                                and more efficient computing experience for both professionals and everyday users.
+                            </p>
+                            <p>
+                                With cutting-edge fabrication technology, Apple M4 runs complex applications with incredible responsiveness 
+                                while maintaining outstanding battery life. Its more powerful GPU enables smooth and realistic graphics.
+                            </p>
+                            <p className="text-primary">
+                                Learn More
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="max-w-3xs space-y-14">
+                        <div className="space-y-2">
+                            <p>Up to</p>
+                            <h3>3x faster</h3>
+                            <p>Rendering performance than M2</p>
+                        </div>
+                        <div className="space-y-2">
+                            <p>Up to</p>
+                            <h3>1.5x faster</h3>
+                            <p>CPU performance than M2</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
